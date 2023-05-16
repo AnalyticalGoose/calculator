3ee6a31a8d009f6104f8e9d1adc1e6ae375def17
Harry <harryedgar@hotmail.co.uk>
2023-05-16 11:47:02 +0100

    complete main functionality

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const buttons = document.querySelectorAll('button')
 const topScreen = document.querySelector('.top-screen')
 const bottomScreen = document.querySelector('.bottom-screen')
 
-let memory = ''
-let operationMemory = ''
+let totalValue = '0'
+let screenValue = ''
+let operatorMemory = '+'
+let tempMemory = ''
 
 window.addEventListener('keydown', function(e) {
-    console.log(e.key)
+    const key = document.querySelector(`button[data-key="${e.key}"`)
+    !key ? false : eventBus(key.innerText)
 })
 
 buttons.forEach(b => {
@@ -16,44 +19,69 @@ buttons.forEach(b => {
 })
 
 function updateInput(input) {
-    bottomScreen.textContent += input
+    screenValue += input
+    bottomScreen.textContent = screenValue
 }
 
-function screenShift(input) {
-    topScreen.textContent = bottomScreen.textContent
-    bottomScreen.textContent = input
+function updateTotal(calc) {
+    totalValue = calc
+    topScreen.textContent = totalValue
 }
 
+function updateScreenOperator() {
+    operatorMemory = tempMemory
+    bottomScreen.textContent = operatorMemory
+    screenValue = ""
+}
 
 function eventBus(input) {
-    !isNaN(input) ? updateInput(input) : operatorFunctions[input]()
+    !isNaN(input) ? updateInput(input) : operatorBus(input)
+}
+
+function operatorBus(input) {
+    ['+', '-', 'x', '÷'].includes(input) ? (tempMemory = input, operatorFunctions[operatorMemory]()) : otherFunctions[input]()
+}
+
+const otherFunctions = {
+    '=': () => displayEvaluation(operations[operatorMemory](totalValue, screenValue)),
+    '%': () => displayEvaluation(operations['x']((parseFloat(screenValue)/100), totalValue)),
+    'AC': () => displayEvaluation(""),
+    '▶': () => (screenValue = screenValue.slice(-0, -1), bottomScreen.textContent = screenValue),
+    '.': () => bottomScreen.textContent.includes('.') ? false : updateInput('.'),
+    '√': () => bottomScreen.textContent.includes('√') ? false : (updateInput('√'), operatorMemory = '√'),
+}
+
+function displayEvaluation(input) {
+    totalValue = "0"
+    operatorMemory = "+"
+    screenValue = input
+    bottomScreen.textContent = screenValue
+    topScreen.textContent = ""
 }
 
 const operatorFunctions = {
-    
-    
-    '+': () => (evaluate(topScreen.textContent, bottomScreen.textContent, 'add'), screenShift('+')),
-
-
-    '-': () => console.log('minus'),
-    'x': () => console.log('multiply'),
-    '÷': () => console.log('divide'),
-    '=': () => console.log('eval'),
-    '.': () => bottomScreen.textContent.includes('.') ? false : updateInput('.'),
-    'AC': () => console.log('clear'),
-    '▶': () => console.log('backspace'),
-    '%': () => console.log('percent'),
-    '√': () => console.log('sqrt'),
+    '+': () => {
+        updateTotal(operations['+'](totalValue, screenValue))
+        updateScreenOperator()
+    },
+    '-': () => {
+        updateTotal(operations['-'](totalValue, screenValue))
+        updateScreenOperator()
+    },
+    'x': () => {
+        updateTotal(operations['x'](totalValue, screenValue))
+        updateScreenOperator()
+    },
+    '÷': () => {
+        updateTotal(operations['÷'](totalValue, screenValue))
+        updateScreenOperator()
+    }
 }
 
-function evaluate(x, y, operation) {
-    const operations = {
-        'add' : () => x + y,
-        // subtract:
-        // multiply:
-        // divide:
-        // percentage:
-        // sqrt:
-    }
-    console.log(operations[operation])
-} 
+const operations = {
+    '+': (x, y) => {return parseFloat((parseFloat(x) + parseFloat(y)).toFixed(3))},
+    '-': (x, y) => {return parseFloat((parseFloat(x) - parseFloat(y)).toFixed(3))},
+    'x': (x, y) => {return parseFloat((parseFloat(x) * parseFloat(y)).toFixed(3))},
+    '÷': (x, y) => {return parseFloat((parseFloat(x) / parseFloat(y)).toFixed(3))},
+    '√': (x, y) => {return parseFloat(Math.sqrt(y.slice(1)).toFixed(3))}
+}
